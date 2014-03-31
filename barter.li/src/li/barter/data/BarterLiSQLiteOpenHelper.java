9c01aa44186ae752bb0e0d1e996fbb5956a19a71
Vinay S Shenoy <vinay.shenoy@sourcebits.com>
2014-03-31 11:57:01 +0530

    Notifying Loaders connected to the Views when the individual tables are updated

--- barter.li/src/li/barter/data/BarterLiSQLiteOpenHelper.java
+++ barter.li/src/li/barter/data/BarterLiSQLiteOpenHelper.java
@@ -89,12 +89,12 @@ class BarterLiSQLiteOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(final SQLiteDatabase db) {
-        
+
         //Create tables
         TableSearchBooks.create(db);
         TableLocations.create(db);
         TableMyBooks.create(db);
-        
+
         //Create Views
         ViewSearchBooksWithLocations.create(db);
     }
@@ -102,12 +102,12 @@ class BarterLiSQLiteOpenHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(final SQLiteDatabase db, final int oldVersion,
                     final int newVersion) {
-        
+
         //Upgrade tables
         TableSearchBooks.upgrade(db, oldVersion, newVersion);
         TableLocations.upgrade(db, oldVersion, newVersion);
         TableMyBooks.upgrade(db, oldVersion, newVersion);
-        
+
         //Upgrade Views
         ViewSearchBooksWithLocations.upgrade(db, oldVersion, newVersion);
     }
@@ -239,7 +239,12 @@ class BarterLiSQLiteOpenHelper extends SQLiteOpenHelper {
         //TODO Optimize this later, Maybe a sorted list of loaders by table name?
         for (final SQLiteLoaderObserver entry : mActiveLoaders) {
             Logger.d(TAG, "Notify change: %s", entry.table);
-            if (entry.table.equals(table)) {
+            /*
+             * Using contains instead of equals because we are using View, which
+             * are just named by appending the table names. Notifying should
+             * also update any Loaders connected to the Views
+             */
+            if (entry.table.contains(table)) {
                 entry.loader.onContentChanged();
             }
         }
