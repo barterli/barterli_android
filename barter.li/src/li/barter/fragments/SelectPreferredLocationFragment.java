d9c7fbe8216dfae64f5ba89d736c86ba3a82fe1c
Vinay S Shenoy <vinaysshenoy@gmail.com>
2014-05-27 19:27:42 +0530

    Added layout for select place, removed Map from select place screen

--- barter.li/src/li/barter/fragments/SelectPreferredLocationFragment.java
+++ barter.li/src/li/barter/fragments/SelectPreferredLocationFragment.java
@@ -17,18 +17,6 @@
 package li.barter.fragments;
 
 import com.android.volley.Request.Method;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.CancelableCallback;
-import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
-import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
-import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -64,9 +52,7 @@ import li.barter.utils.SharedPreferenceHelper;
  *         exchanging books
  */
 @FragmentTransition(enterAnimation = R.anim.slide_in_from_right, exitAnimation = R.anim.zoom_out, popEnterAnimation = R.anim.zoom_in, popExitAnimation = R.anim.slide_out_to_right)
-public class SelectPreferredLocationFragment extends AbstractBarterLiFragment
-                implements CancelableCallback, OnInfoWindowClickListener,
-                OnMarkerDragListener, OnMarkerClickListener {
+public class SelectPreferredLocationFragment extends AbstractBarterLiFragment {
 
     private static final String  TAG                      = "SelectPreferredLocationFragment";
 
@@ -82,45 +68,9 @@ public class SelectPreferredLocationFragment extends AbstractBarterLiFragment
     private static final String  FOURSQUARE_API_VERSION   = "20140526";
 
     /**
-     * Zoom level for the map when the location is retrieved
-     */
-    private static final float   MAP_ZOOM_LEVEL           = 15;
-
-    /**
-     * Hue for custom marker, to match with the app theme
-     */
-    private final float          mCustomMarkerHue         = 196.0f;
-
-    /**
-     * {@link MapView} used to display the Map
-     */
-    private MapView              mMapView;
-
-    /**
      * {@link Venue}s used to place nearby marker locations on Map
      */
     private Venue[]            mHangouts;
-
-    /**
-     * {@link Marker} that can be used to set a custom location
-     */
-    private Marker               mCustomMarker;
-
-    /**
-     * Title format for the marker
-     */
-    private final String         mMarkerTitleFormat       = "%s, %s";
-
-    /**
-     * Title format for the custom marker
-     */
-    private final String         mCustomMarkerTitleFormat = "%.5f, %.5f";
-
-    /**
-     * A 1-to-1 mapping between the Map markers and the hangouts to resolve
-     * which Hangout was selected when user tapped the marker info window
-     */
-    private Map<Marker, Venue> mMarkerHangoutMap;
 
     private String               mFoursquareClientId;
 
@@ -137,22 +87,7 @@ public class SelectPreferredLocationFragment extends AbstractBarterLiFragment
         showInfiniteCrouton(R.string.crouton_prefferedlocation_message, AlertStyle.INFO);
         mFoursquareClientId = getString(R.string.foursquare_client_id);
         mFoursquareClientSecret = getString(R.string.foursquare_client_secret);
-
-        mMarkerHangoutMap = new HashMap<Marker, Venue>();
-        /*
-         * The Google Maps V2 API states that when using MapView, we need to
-         * forward the onCreate(Bundle) method to the MapView, but since we are
-         * in a fragment, the onCreateView() gets called AFTER the
-         * onCreate(Bundle) method, which makes forwarding that method
-         * impossible. This is the workaround for that
-         */
-        MapsInitializer.initialize(getActivity());
-        mMapView = (MapView) contentView
-                        .findViewById(R.id.map_preferred_location);
-        mMapView.onCreate(savedInstanceState);
-        setUpMapListeners();
-        moveMapToLocation(DeviceInfo.INSTANCE.getLatestLocation());
-
+       
         if (savedInstanceState != null) {
             mHangouts = (Venue[]) savedInstanceState
                             .getParcelableArray(Keys.LOCATIONS);
@@ -161,7 +96,7 @@ public class SelectPreferredLocationFragment extends AbstractBarterLiFragment
         if ((mHangouts == null) || (mHangouts.length == 0)) {
             fetchVenuesForLocation(DeviceInfo.INSTANCE.getLatestLocation(), SEARCH_RADIUS_IN_METERS);
         } else {
-            addMarkersToMap(mHangouts);
+            //TODO Load Venues in List
         }
         setActionBarDrawerToggleEnabled(false);
         return contentView;
@@ -213,9 +148,6 @@ public class SelectPreferredLocationFragment extends AbstractBarterLiFragment
     @Override
     public void onSaveInstanceState(final Bundle outState) {
         super.onSaveInstanceState(outState);
-        if (mMapView != null) {
-            mMapView.onSaveInstanceState(outState);
-        }
         outState.putParcelableArray(Keys.LOCATIONS, mHangouts);
     }
 
@@ -224,76 +156,10 @@ public class SelectPreferredLocationFragment extends AbstractBarterLiFragment
         return TAG;
     }
 
-    /**
-     * Moves the map to a location
-     * 
-     * @param location The {@link Location} to move the map to
-     */
-    private void moveMapToLocation(final Location location) {
-
-        /*
-         * For the initial launch, move the Map to the user's current position
-         * as soon as the location is fetched
-         */
-        final GoogleMap googleMap = getMap();
-
-        if (googleMap != null) {
-            googleMap.setMyLocationEnabled(false);
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location
-                            .getLatitude(), location.getLongitude()), MAP_ZOOM_LEVEL), this);
-
-        }
-    }
-
     @Override
     public void onPause() {
         super.onPause();
         cancelAllCroutons();
-        mMapView.onPause();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        mMapView.onResume();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (mMapView != null) {
-            mMapView.onDestroy();
-        }
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        if (mMapView != null) {
-            mMapView.onLowMemory();
-        }
-    }
-
-    /**
-     * Sets up the listeners for the Map
-     */
-    private void setUpMapListeners() {
-        final GoogleMap map = getMap();
-
-        if (map != null) {
-            map.setOnInfoWindowClickListener(this);
-            map.setOnMarkerDragListener(this);
-        }
-    }
-
-    @Override
-    public void onCancel() {
-
-    }
-
-    @Override
-    public void onFinish() {
-        //Draw the markers
     }
 
     @Override
@@ -303,7 +169,6 @@ public class SelectPreferredLocationFragment extends AbstractBarterLiFragment
         if (requestId == RequestId.FOURSQUARE_VENUES) {
             mHangouts = (Venue[]) response.responseBundle
                             .getParcelableArray(HttpConstants.LOCATIONS);
-            addMarkersToMap(mHangouts);
         } else if (requestId == RequestId.SET_USER_PREFERRED_LOCATION) {
 
             SharedPreferenceHelper
@@ -337,69 +202,6 @@ public class SelectPreferredLocationFragment extends AbstractBarterLiFragment
     }
 
     /**
-     * Parses the LatLng values from the Hangouts passed, adds Map markers
-     */
-    private void addMarkersToMap(final Venue[] hangouts) {
-        final GoogleMap map = getMap();
-        map.setOnMarkerClickListener(this);
-        if (map != null) {
-            mMarkerHangoutMap.clear();
-
-            Marker marker = null;
-            for (final Venue aHangout : hangouts) {
-
-                marker = map.addMarker(new MarkerOptions()
-                                .position(new LatLng(aHangout.latitude, aHangout.longitude))
-                                .title(String.format(mMarkerTitleFormat, aHangout.name, aHangout.address))
-                                .snippet(getString(R.string.tap_to_set_preferred_location)));
-
-                mMarkerHangoutMap.put(marker, aHangout);
-
-            }
-
-            if (mCustomMarker == null) {
-                final Location location = DeviceInfo.INSTANCE
-                                .getLatestLocation();
-                mCustomMarker = map
-                                .addMarker(new MarkerOptions()
-                                                .draggable(true)
-                                                .icon(BitmapDescriptorFactory
-                                                                .defaultMarker(mCustomMarkerHue))
-                                                .title(String.format(mCustomMarkerTitleFormat, location
-                                                                .getLatitude(), location
-                                                                .getLongitude()))
-
-                                                .position(new LatLng(location
-                                                                .getLatitude(), location
-                                                                .getLongitude())));
-            }
-        }
-    }
-
-    /**
-     * Gets a reference of the Google Map
-     */
-    private GoogleMap getMap() {
-
-        return mMapView.getMap();
-    }
-
-    @Override
-    public void onInfoWindowClick(final Marker marker) {
-
-        if (marker.equals(mCustomMarker)) {
-            final LatLng markerPosition = marker.getPosition();
-            Logger.v(TAG, "Custom Marker Clicked - Latitude %f Longitude %f", markerPosition.latitude, markerPosition.longitude);
-            //TODO Reverse geocode the marker position to get the location info
-        } else {
-
-            final Venue selectedHangout = mMarkerHangoutMap.get(marker);
-            Logger.v(TAG, "Marker Clicked: %s, %s", selectedHangout.name, selectedHangout.address);
-            setUserPreferredLocation(selectedHangout.name, selectedHangout.address, selectedHangout.latitude, selectedHangout.longitude);
-        }
-    }
-
-    /**
      * Uploads the user preferred location to server
      * 
      * @param name The name of the location
@@ -428,35 +230,4 @@ public class SelectPreferredLocationFragment extends AbstractBarterLiFragment
 
     }
 
-    @Override
-    public void onMarkerDrag(final Marker marker) {
-
-    }
-
-    @Override
-    public void onMarkerDragEnd(final Marker marker) {
-        if (marker.equals(mCustomMarker)) {
-            final LatLng position = marker.getPosition();
-            marker.setTitle(String
-                            .format(mCustomMarkerTitleFormat, position.latitude, position.longitude));
-            marker.showInfoWindow();
-        }
-    }
-
-    @Override
-    public void onMarkerDragStart(final Marker marker) {
-
-    }
-
-    @Override
-    public boolean onMarkerClick(final Marker marker) {
-
-        if (marker.equals(mCustomMarker)) {
-            final LatLng markerPosition = marker.getPosition();
-            Logger.v(TAG, "Custom Marker Clicked - Latitude %f Longitude %f", markerPosition.latitude, markerPosition.longitude);
-            //TODO Reverse geocode the marker position to get the location info
-        }
-
-        return false;
-    }
 }
