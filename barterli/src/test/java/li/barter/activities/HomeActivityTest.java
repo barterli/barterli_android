15f4a195861845454346bd266c24786c5e5e41bc
mipreamble <imohammed23@gmail.com>
2014-06-30 08:40:14 +0530

    Some more testing

--- barterli/src/test/java/li/barter/activities/HomeActivityTest.java
+++ barterli/src/test/java/li/barter/activities/HomeActivityTest.java
@@ -1,14 +1,23 @@
 package li.barter.activities;
 
+import android.widget.TextView;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import li.barter.R;
+
+import static org.fest.assertions.api.ANDROID.assertThat;
 
 /**
  * Created by imran on 30/06/14.
  */
 
+@Config(emulateSdk = 18)
 @RunWith(RobolectricTestRunner.class)
 public class HomeActivityTest {
 
@@ -19,6 +28,13 @@ public class HomeActivityTest {
         activity = Robolectric.buildActivity(HomeActivity.class).create().visible().get();
 
         //assertTrue(activity != null);
+    }
+
+    @Test
+    public void sampleTest() {
+        TextView textView = (TextView) activity.findViewById(R.id.hello_world);
+
+        assertThat(textView).containsText("Hello");
     }
 
 
--- barterli/src/test/java/li/barter/fragments/BooksAroundMeFragmentTest.java
+++ barterli/src/test/java/li/barter/fragments/BooksAroundMeFragmentTest.java
@@ -1,18 +1,28 @@
 package li.barter.fragments;
 
+import android.widget.GridView;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.util.FragmentTestUtil;
+
+import li.barter.R;
+
+import static org.fest.assertions.api.ANDROID.assertThat;
 
 /**
  * Created by imran on 30/06/14.
  */
 
+@Config(emulateSdk = 18)
 @RunWith(RobolectricTestRunner.class)
-public class BooksAroundMeFragmentTest extends FragmentBase<BooksAroundMeFragment> {
+public class BooksAroundMeFragmentTest {
 
     private BooksAroundMeFragment fragment;
+    private GridView gridView;
 
     @Before
     public void setUp() {
@@ -20,19 +30,11 @@ public class BooksAroundMeFragmentTest extends FragmentBase<BooksAroundMeFragmen
     }
 
     @Test
-    public void createsAndDestroysFragment() {
-        startFragment(fragment);
+    public void createsAndDestroysFragment() throws Exception {
+        FragmentTestUtil.startFragment(fragment);
+        assertThat(fragment).isNotNull();
+        gridView = (GridView) fragment.getActivity().findViewById(R.id.grid_books_around_me);
+        assertThat(gridView).isNotNull();
     }
 
-    @Test
-    public void pausesAndResumesFragment() {
-        startFragment(fragment);
-        pauseAndResumeFragment();
-    }
-
-    @Test
-    public void recreatesFragment() {
-        startFragment(fragment);
-        fragment = recreateFragment();
-    }
 }
